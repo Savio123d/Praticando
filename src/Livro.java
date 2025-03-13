8fa80ccbf61465926b7c8c65607493cd74a372b0
Savio123d <savio13foxy@gmail.com>
2025-03-12 21:44:47 -0300

    testando

--- src/Livro.java
+++ src/Livro.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Livro {
-    public String nomeL,genero;
+    public String[] genero;
     public int verL;
+    Scanner Scanner = new Scanner(System.in);
+    public String[] nomeL = new String[20];
 
-
+    public  void Clivro(){
+     System.out.println("Informe nome do livro quer cadastrar: ");
+     nomeL[0] = Scanner.next();
+   }
 }
